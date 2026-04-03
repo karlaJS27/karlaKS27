2d7e9e4dbb7a1de779db866b769a1a45fbd1be16
karlaJS27 <cielo_20041@hotmail.com>
2026-04-02 19:36:52 -0600

    Actualizar proyecto mcp01: mejoras en main.ts y agregar devDependencies

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 
 import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
-import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio";
+import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
 import { z } from 'zod'
 
 // 1. Crear el servidor MCP
@@ -20,14 +20,34 @@ server.tool(
     },
 
     async({ city }) => {
+        const response = await fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${city}&count=10&language=es&format=json`)
+        const data = await response.json()
+
+        if (data.length === 0) {
         return { 
         content: [
                     {
                         type: 'text',
-                        text: `The weather in ${city} is sunny with a temperature of 25°C.`
+                        text: `No se encontro informacion de la  ${city}`
                     }
                 ]
             }
+        }
+
+        const { latitude, longitude } = data.results[0]
+        const weatherResponse = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m&current=temperature_2m,is_day,rain,showers,precipitation&forecast_days=1`)
+        const weatherData = await weatherResponse.json()
+
+
+        return {
+            content: [
+                {
+                    type: 'text',
+                    text: JSON.stringify(weatherData, null, 2)
+                }
+            ]
+        }
+
     }
 )
 
